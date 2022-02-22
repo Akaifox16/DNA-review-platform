82e45394729ba40973e07089793767c562140a9f
mook30888 <mookkak@gmail.com>
2022-02-22 20:37:58 +0700

    add little community front

---
+++ components/Community/CommunityCard.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import { Col, Container, Row, Button, Stack } from "react-bootstrap";
+import { PostCardProps } from "../../lib/type";
+
+const PostCard = ({ id, owner, title }: PostCardProps) => {
+    return (
+        <Stack direction="horizontal" gap={3}>
+            <Image src='/vercel.svg' width={124} height={124}/>
+            <Stack gap={3}>
+                <div>Product Name: { title }</div>
+                <div>Score: </div>
+                <div>Reviewer: { owner }</div>
+            </Stack>
+        </Stack>
+    );
+}
+
+export default PostCard;
---
+++ components/Community/index.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import { Button, Stack } from "react-bootstrap";
+import { PostsDetailProps } from "../../lib/type";
+// import PostCard from "./PostCard";
+
+const PostList = ({ postsDetail }:PostsDetailProps) => {
+    return (
+        <div>
+            <Stack gap={3}>
+                {
+                    postsDetail.map(post => {
+                        const { id, title, owner} = post
+                        return (
+                            <Link href={`/post/${title}`} > 
+                                <Button variant="outline-primary">
+                                    {/* <PostCard id={id} title={title} owner={owner} /> */}
+                                </Button>
+                            </Link>
+                        );
+                    })
+                }
+            </Stack>
+        </div>
+    );
+}
+
+
+
+export default PostList;
