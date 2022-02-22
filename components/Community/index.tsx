b103adab667e777ed3e6038454bc448a15bc727b
mook30888 <mookkak@gmail.com>
2022-02-22 23:53:36 +0700

    idk what is going on in community

--- components/Community/index.tsx
+++ components/Community/index.tsx
@@ -3,22 +3,23 @@ import { Button, Stack } from "react-bootstrap";
 import { PostsDetailProps } from "../../lib/type";
 import { CommunityDetailProps } from "../../lib/type/props";
 import PostCard from "../Post/PostCard";
+import CommunityCard from "./CommunityCard";
 
 const CommuPostList = ({ commusDetail }:CommunityDetailProps) => {
     return (
         <div>
             <Stack gap={3}>
                 {
-                    commusDetail.map(post => {
-                        const { id, name, tags} = post
-                        return (
-                            <Link href={`/post/${name}`} > 
+                    // commusDetail.map(post => {
+                        // const { id, name, tags} = post
+                        // return (
+                            <Link href={`/post/ปวดขี้`} > 
                                 <Button variant="outline-primary">
-                                    <PostCard id={id} title={name} owner={"owner"} />
+                                    <CommunityCard id={12} title={"ปวดขี้"} owner={"owner"} />
                                 </Button>
                             </Link>
-                        );
-                    })
+                        // );
+                    // })
                 }
             </Stack>
         </div>
